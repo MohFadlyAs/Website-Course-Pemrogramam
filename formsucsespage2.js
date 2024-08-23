0494ff5708a93061bcfb155cbf11998976aecfa8
MohFadlyAs <fadlyahmadsehabudin@gmail.com>
2024-08-23 17:50:27 +0700

    Add files via upload

---
+++ formsucsespage2.js
@@ -0,0 +1,11 @@
+// Jika diperlukan, Anda bisa menambahkan interaktivitas tambahan di sini.
+// Misalnya, untuk animasi atau redirection setelah beberapa detik.
+$(document).ready(function() {
+    // Contoh redirect otomatis setelah 5 detik
+    setTimeout(function() {
+        window.location.href = "index.html";
+    }, 5000);
+});
+
+
+
